516fb94323ecfb8d413c1e85df6fe5e2375e9339
Loacust <97564364+Loacust@users.noreply.github.com>
2022-04-13 15:23:28 -0600

    DoneTEsting

--- Module 6/tabsapp/src/app/tab1/tab1.page.ts
+++ Module 6/tabsapp/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+
+  async showAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'This is a friendly reminder',
+      message: 'To go fuck yourself',
+      buttons: [
+        {text:"OK",
+      handler: ()=> {
+        console.log("OK button was clicked")
+      }},
+      {
+        text: "Cancel",
+        handler:()=> {
+          console.log("Cancel button was clicked")
+        }
+      }
+      ]
+    });
+    await alert.present();
+  }
 }
--- Module 6/tabsapp/src/app/tab3/tab3.page.ts
+++ Module 6/tabsapp/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
+  async showToast() {
+    const toast = await this.toastController.create({
+      header: 'KILLING IT!',
+      message: '',
+      icon: '',
+      position: 'middle',
+      animated: true,
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    await toast.present();
+
+    const { role } = await toast.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 
 }
