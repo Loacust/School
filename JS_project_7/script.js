4a721cae673a2c42d44dbf9dbb4f110a548a4b0c
Loacust <97564364+Loacust@users.noreply.github.com>
2022-04-15 15:48:30 -0600

    Complete

---
+++ JS_project_7/script.js
@@ -0,0 +1,15 @@
+
+
+
+function sumNumbers(){
+    var x = 0;
+    let i = 0;
+    for(i; i<=100; i++){
+     x = x + i;
+    };
+    return x;
+};
+function displayTotal(){
+    console.log(sumNumbers());
+};
+
